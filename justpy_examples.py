c4901afe881db3c48823ab11fe00b8525e7ad9d0
Chris Billows <chrisbillows@Chriss-MacBook-Pro.local>
2023-03-16 15:12:57 +0000

    Add page level div
    And attach elements to page_div for consistent styling

--- justpy_examples.py
+++ justpy_examples.py
@@ -5,18 +5,21 @@ import justpy as jp
 def homepage():
     wp = jp.WebPage()   # instantiate a WebPage instance
 
-    # add styling as tailwind css to classes (need space after split lines)
-    jp.Div(a=wp, classes="bg-gray-200")
-    jp.Input(a=wp, placeholder="Enter first value",
+    # create div for page level styling
+    page_div = jp.Div(a=wp, classes="bg-gray-200 h-screen")  # h = height
+
+    # classes - add styling as tailwind css (need space after split classes lines)
+    jp.Input(a=page_div, placeholder="Enter first value",
              classes="form-input")
-    jp.Input(a=wp, placeholder="Enter second value",
+    jp.Input(a=page_div, placeholder="Enter second value",
              classes="form-input")
-    jp.Div(a=wp, text="Result goes here...", classes="text-gray-600")
-    jp.Button(a=wp, text="Calculate",
+    jp.Div(a=page_div, text="Result goes here...", classes="text-gray-600")
+    jp.Button(a=page_div, text="Calculate",
               classes="border border-blue-500 m-2 py-1 px-4 rounded "
-              "text-blue-600 hover:bg-red-500 hover:text-white") # m = margin, p = padding
+              "text-blue-600 hover:bg-red-500 hover:text-white")  # m = margin, p = padding
     return wp
 
 # jp.Route("/", homepage)  # creates a route with a function
+
 
 jp.justpy()
