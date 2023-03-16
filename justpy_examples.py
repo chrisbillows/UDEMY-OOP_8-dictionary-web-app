1e86d58422a0949bd07b92374a271960c4a0bd64
Chris Billows <chrisbillows@Chriss-MacBook-Pro.local>
2023-03-16 15:24:03 +0000

    Add grid layout to justpy_examples

--- justpy_examples.py
+++ justpy_examples.py
@@ -5,18 +5,27 @@ import justpy as jp
 def homepage():
     wp = jp.WebPage()   # instantiate a WebPage instance
 
+    # 'classes' -
+    # add styling as tailwind css (need space after split classes lines)
+
     # create div for page level styling
     page_div = jp.Div(a=wp, classes="bg-gray-200 h-screen")  # h = height
 
-    # classes - add styling as tailwind css (need space after split classes lines)
-    jp.Input(a=page_div, placeholder="Enter first value",
+    # organise elements into grid
+    div1 = jp.Div(a=page_div, classes="grid grid-cols-3 gap-4 p-4")
+    jp.Input(a=div1, placeholder="Enter first value",
              classes="form-input")
-    jp.Input(a=page_div, placeholder="Enter second value",
+    jp.Input(a=div1, placeholder="Enter second value",
              classes="form-input")
-    jp.Div(a=page_div, text="Result goes here...", classes="text-gray-600")
-    jp.Button(a=page_div, text="Calculate",
+    jp.Div(a=div1, text="Result goes here...", classes="text-gray-600")
+    jp.Div(a=div1, text="Just another div...", classes="text-gray-600")
+    jp.Div(a=div1, text="Yet another div...", classes="text-gray-600")
+
+    div2 = jp.Div(a=page_div, classes="grid grid-cols-2 gap-4")
+    jp.Button(a=div2, text="Calculate",
               classes="border border-blue-500 m-2 py-1 px-4 rounded "
               "text-blue-600 hover:bg-red-500 hover:text-white")  # m = margin, p = padding
+    jp.Div(a=div2, text="I am a cool interactive div!", classes="text-gray-600")
     return wp
 
 # jp.Route("/", homepage)  # creates a route with a function
