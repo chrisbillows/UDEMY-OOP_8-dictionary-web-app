21a3c1fd7abc4d7567847ab6e15e4b193b8a072c
Chris Billows <christopherbillows@gmail.com>
2023-03-17 14:28:58 +0000

    Add content to dictionary page
    Includes input object. For class dictionary use class method decorator. Add pycache to gitignore.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import justpy as jp
+
 from webapp.about import About
+from webapp.dictionary import Dictionary
 from webapp.home import Home
 
 
 jp.Route(About.path, About.serve)
+jp.Route(Dictionary.path, Dictionary.serve)
 jp.Route(Home.path, Home.serve)
 jp.justpy()
 
